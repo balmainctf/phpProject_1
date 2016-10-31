654fa568fd9777279be8d0d9d31fd535caf3b3d8
marshal <kidsover200891@qq.com>
2016-11-01 07:09:11 +0800

    更新了验证码

--- login.php
+++ login.php
@@ -1,35 +1,42 @@
 <?php
 header("content-type:text/html;charset=utf8");
+session_start();
 $username = $_POST['username'];
 $password = md5($_POST['password']);
+$verify = strtolower($_POST['verify']);
+if($_SESSION['verify'] == $verify){
+	echo "hello";
+	exit();
+}
 
-$link=mysql_connect('localhost','root','123');
+// $link=mysql_connect('localhost','root','123');
 
-mysql_set_charset("utf8");
+// mysql_set_charset("utf8");
 
-mysql_select_db('pra');
+// mysql_select_db('pra');
 
-$sql = "select * from admin where username='{$username}' and password='{$password}'";
+// $sql = "select * from admin where username='{$username}' and password='{$password}'";
 
-// echo $sql;
+// // echo $sql;
+// // exit();
+// $re = mysql_query($sql);
+// if(!$re){
+// 	die(mysql_errno());
+// }
+// $arr = mysql_fetch_assoc($re);
+
+// if(!empty($arr)){
+// 	//创建cookie
+// 	//setcookie('adminid',$arr['id'],0,'/');
+// 	//setcookie('username',$username,0,'/');
+// 	//创建session
+
+// 	$_SESSION['adminid'] = $arr['id'];
+// 	$_SESSION['username'] = $arr['username'];
+	
+// 	header("location:index.php");
+// }else{
+// 	header("location:login.html");
+// }
+// var_dump($arr);
 // exit();
-$re = mysql_query($sql);
-if(!$re){
-	die(mysql_errno());
-}
-$arr = mysql_fetch_assoc($re);
-
-if(!empty($arr)){
-	//创建cookie
-	//setcookie('adminid',$arr['id'],0,'/');
-	//setcookie('username',$username,0,'/');
-	//创建session
-	session_start();
-	$_SESSION['adminid'] = $arr['id'];
-	$_SESSION['username'] = $arr['username'];
-	header("location:index.php");
-}else{
-	header("location:login.html");
-}
-var_dump($arr);
-exit();
